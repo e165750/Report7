b417da44609b27d6634a03011b837c418ccb7dc5
Wakana Iha <e165750@ie.u-ryukyu.ac.jp>
2017-02-05 23:01:13 +0900

    骨組みのコードは完成

--- src/main/java/jp/ac/uryukyu/ie/e165750/Destroy.java
+++ src/main/java/jp/ac/uryukyu/ie/e165750/Destroy.java
@@ -1,120 +1,150 @@
 package jp.ac.uryukyu.ie.e165750;
-import java.awt.event.KeyEvent;
-
-
-
+import java.io.*;
 /**
  * Created by e165750 on 2017/01/28.
  */
 public class Destroy {
+
     int x, y;//スイカの位置をxy座標で表す
     int f, p;//プレイヤーの初期値を表す
-    //suikaserchクラスでもこれらの変数は使用したいのでフィールド変数とした
+
+    public Destroy() throws IOException {
+    }
 
     public void suikaposition(Destroy e) {//スイカとプレイヤーの初期値を保存するメソッド
 
         String sand[][];//sandという名前の配列を用意
 
-        sand = new String[10][10];//この配列に格納する値をString型に設定、また配列のサイズをを10*10に設定した
-        x = (int) (Math.random() * 9);
-        y = (int) (Math.random() * 9);
+        sand = new String[10][10];//この配列に格納する値をString型に設定、また配列のサイズを10*10に設定した
 
         sand[x][y] = "suika";//適当な場所にスイカを置く
-        //System.out.println(sand[x][y]);
-        //System.out.println(x);
-        //System.out.println(y);
 
-        e.syokiti();
-
-        if (f == x && p == y) {//初期値がスイカの場所と被らないようにするためのif文
-            e.syokiti();
-        }
         sand[f][p] = "syokiti";//初期値を適当なところに配置
-        //System.out.println(sand[f][p]);
-        //System.out.println(f);
-        //System.out.println(p);
-        }
+
+    }
 
     public void syokiti() {//プレイヤーの初期値を決めるメソッド
         f = (int) (Math.random() * 9);
         p = (int) (Math.random() * 9);
     }
+    int length;
+    int tatesa;
+    int width;
+    int yokosa;
 
-
-
-        public int getHint() {//スイカと初期値の距離によってヒントを与えるメソッド
-            int length = f - x;//・・・(1)
-            int tatesa = Math.abs(length);//(1)とこのコードでスイカと初期値の縦の距離を測る
-            int width = p - y;//・・・(2)
-            int yokosa = Math.abs(width);//(2)とこのコードでスイカと初期値の横の距離を測る
-            if (x > f) {
-                if (tatesa < 2) {
-                    System.out.println("もう少し上!!");
-                } else if (tatesa < 5) {
-                    System.out.println("まぁまぁ上!!");
-                } else if (tatesa >= 5) {
-                    System.out.println("ずっと上!!");
-                }
-            } else {
-                if (tatesa < 2) {
-                    System.out.println("もう少し下!!");
-                } else if (tatesa < 5) {
-                    System.out.println("まぁまぁ下!!");
-                } else if (tatesa >= 5) {
-                    System.out.println("ずっと下!!");
-                }
-
+    public int getHint() {//スイカと初期値の距離によってヒントを与えるメソッド
+        if (x > f) {
+            if (yokosa < 2) {
+                System.out.println("もう少し上!!");
+            } else if (yokosa < 5) {
+                System.out.println("まぁまぁ上!!");
+            } else if (yokosa >= 5) {
+                System.out.println("ずっと上!!");
             }
-            if (y > p) {
-                if (yokosa < 2) {
-                    System.out.println("もう少し左!!");
-                } else if (yokosa < 5) {
-                    System.out.println("まぁまぁ左!!");
-                } else if (yokosa >= 5) {
-                    System.out.println("ずっと左!!");
-                }
-            } else {
-                if (yokosa < 2) {
-                    System.out.println("もう少し右!!");
-                } else if (yokosa < 5) {
-                    System.out.println("まぁまぁ右!!");
-                } else if (yokosa >= 5) {
-                    System.out.println("ずっと右!!");
-                }
+        } else {
+            if (yokosa < 2) {
+                System.out.println("もう少し下!!");
+            } else if (yokosa < 5) {
+                System.out.println("まぁまぁ下!!");
+            } else if (yokosa >= 5) {
+                System.out.println("ずっと下!!");
             }
 
-            return 0;
+        }
+        if (y > p) {
+            if (tatesa < 2) {
+                System.out.println("もう少し左!!");
+            } else if (tatesa < 5) {
+                System.out.println("まぁまぁ左!!");
+            } else if (tatesa >= 5) {
+                System.out.println("ずっと左!!");
+            }
+        } else {
+            if (tatesa < 2) {
+                System.out.println("もう少し右!!");
+            } else if (tatesa < 5) {
+                System.out.println("まぁまぁ右!!");
+            } else if (tatesa >= 5) {
+                System.out.println("ずっと右!!");
+            }
         }
 
-
-    public void keyPressed(KeyEvent evt) {
-        if (evt.getKeyCode() == KeyEvent.VK_UP) {// 矢印↑キーが押された時の処理
-            f += 1;
-            //System.out.println(x);
-            if (f == 9) ;
-            System.out.println("これ以上進めないよ!");
-        }
-        else if (evt.getKeyCode() == KeyEvent.VK_DOWN) {// 矢印↓キーが押された時の処理
-            f -= 1;
-            //System.out.println(x);
-            if (f == 0) ;
-            System.out.println("これ以上進めないよ!");
-        }
-        else if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {// 矢印→キーが押された時の処理
-            p += 1;
-            //System.out.println(x);
-            if (p == 9) ;
-            System.out.println("これ以上進めないよ!");
-        }
-        else if (evt.getKeyCode() == KeyEvent.VK_LEFT) {// 矢印←キーが押された時の処理
-            p -= 1;
-            //System.out.println(x);
-            if (p == 0) ;
-            System.out.println("これ以上進めないよ!");
-        }
-
+        return 0;
 
     }
+
+         InputStreamReader is = new InputStreamReader(System.in);
+    int charCode = is.read();
+
+     /*
+        キーコード一覧
+        ¥キー；165
+        enterキー；10
+        a；97
+        s；115;
+        z；122
+        x；120
+         */
+
+    public void serchsuika() throws IOException {
+        while (charCode != 165) {//¥が押されるまでこの処理を続ける
+            if (charCode == 97&&f<9) {// a(右)が押された時の処理
+                f += 1;
+                if (f == 9) {
+                    System.out.println("これ以上進めないよ!");
+                }
+
+            }
+            else if (charCode == 115&&f>0) {// s(左)キーが押された時の処理
+                f -= 1;
+                if (f == 0) {
+                    System.out.println("これ以上進めないよ!");
+                }
+
+            }
+            else if (charCode == 120&&p<9) {// x(上)キーが押された時の処理
+                p += 1;
+                if (p == 9) {
+                    System.out.println("これ以上進めないよ!");
+                }
+                if (charCode == 10) {
+                    continue;
+                }
+            }
+            else if (charCode == 122&&p>0) {// z(下)キーが押された時の処理
+                p -= 1;
+                if (p == 0) {
+                    System.out.println("これ以上進めないよ!");
+                }
+            }
+            else if(charCode == 165) {
+                break;
+            }
+            else if (charCode == 10){
+                System.out.println("ザッ");
+            }
+            else {
+                System.out.println("ミスタイプだよ");
+            }
+
+            charCode = is.read();
+        }
+
+    }
+
+
+    public void ¥presssed(){
+        if (tatesa + yokosa < 2) {
+            System.out.println("惜しい!!");
+        } else if (tatesa + yokosa < 5) {
+            System.out.println("もう少し!!");
+        } else if(tatesa + yokosa < 10){
+            System.out.println("まだまだ遠い..");
+        } else if (tatesa + yokosa > 10) {
+            System.out.println("全然遠い!!");
+        }
+    }
+
 
 }
 
--- src/main/java/jp/ac/uryukyu/ie/e165750/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165750/Main.java
@@ -1,20 +1,41 @@
 package jp.ac.uryukyu.ie.e165750;
-import java.awt.event.KeyEvent;
+
+import java.io.IOException;
 
 /**
  * Created by e165750 on 2017/01/28.
  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("エンターを押してね");
+
         Destroy destroy = new Destroy();
-        Destroy e;
+
+
+        destroy.x = (int) (Math.random() * 9);
+        destroy.y  =(int) (Math.random() * 9);
+        destroy.f = (int) (Math.random() * 9);
+        destroy.p  =(int) (Math.random() * 9);
+
         destroy.suikaposition(destroy);//これでスイカの位置とプレイヤーの初期値を配置
-        destroy.getHint();
 
+          if (destroy.f == destroy.x && destroy.p == destroy.y) {//初期値がスイカの場所と被らないようにするためのif文
+            destroy.syokiti();
+        }
 
+        destroy.length = destroy.f - destroy.x;//・・・(1)
+        destroy.tatesa = Math.abs(destroy.length);//(1)とこのコードでスイカと初期値の縦の距離を測る
+        destroy.width = destroy.p - destroy.y;//・・・(2)
+        destroy.yokosa = Math.abs(destroy.width);//(2)とこのコードでスイカと初期値の横の距離を測る
 
+        System.out.println("スイカの位置は...");
 
+        destroy.getHint();//スイカと初期値の距離によってヒントを与えるメソッド
+
+        destroy.serchsuika();//スイカを探すメソッド
+
+        destroy.¥presssed();//¥キーでスイカはここだ、と決めた時のアクションメソッド
     }
 }
 
