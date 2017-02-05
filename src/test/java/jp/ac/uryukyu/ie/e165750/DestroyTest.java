be475a7f670c9198f38d20ca7ee2a8c34b29ff30
Wakana Iha <e165750@ie.u-ryukyu.ac.jp>
2017-02-05 22:09:16 +0900

    ほぼ完成形

---
+++ src/test/java/jp/ac/uryukyu/ie/e165750/DestroyTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e165750;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165750 on 2017/02/05.
+ */
+public class DestroyTest {
+    @Test
+    public void ¥presssed() throws Exception {//tatesa+yokosaの値がヌルではないかどうかのテスト
+        int tatesa = 0;
+        int yokosa = 0;
+
+        assertNotNull(tatesa+yokosa);
+
+
+    }
+
+}
